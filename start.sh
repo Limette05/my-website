584383d9ae924a186db34ea690a7d31a7ad6fd02
Michael Fix <michaelfix@outlook.de>
2025-08-09 15:51:32 +0200

    start skript updated

--- start.sh
+++ start.sh
@@ -4,20 +4,36 @@
 VENV_DIR=".venv"
 
 # Name der Screen-Session
-SCREEN_NAME="flask_app"
+SCREEN_NAME="flask_website"
 
+# IP Adresse des Web-Servers
 HOST="localhost"
+
+RED='\033[0;31m'
+YEL='\033[1;33m'
+
+echo "${YEL}Diese Web-App nutzt die Python Version 3.12!!! Bitte sichergehen!"
+sleep 4s
 
 # Überprüfen, ob screen installiert ist
 if ! command -v screen &> /dev/null; then
-    echo "Error: screen ist nicht installiert. Bitte installiere es mit 'sudo apt-get install screen'"
+    echo "${RED}Error: screen ist nicht installiert. Bitte installiere es mit 'sudo apt-get install screen' und versuch es erneut"
+    exit 1
+fi
+
+# Überprüfen, ob pip installiert ist
+if ! command -v pip &> /dev/null; then
+    echo "${RED}Error: pip ist nicht installiert. Bitte installiere es mit 'sudo apt-get install pip' und versuch es erneut"
     exit 1
 fi
 
 # Virtuelle Umgebung erstellen, falls sie nicht existiert
 if [ ! -d "$VENV_DIR" ]; then
+    echo "${YEL}Installiere python3.12-venv. Bei Fehler versuche: 'sudo apt install python3.12-venv'"
+    sleep 4s
+    apt install python3.12-venv
     echo "Erstelle virtuelle Umgebung..."
-    python3 -m venv $VENV_DIR
+    python3.12 -m venv $VENV_DIR
 fi
 
 # Aktivieren der virtuellen Umgebung
@@ -30,9 +46,10 @@ source $VENV_DIR/bin/activate
 # Überprüfen, ob die Screen-Session bereits läuft
 if screen -list | grep -q "$SCREEN_NAME"; then
     echo "Screen-Session '$SCREEN_NAME' läuft bereits. Schließe..."
+    pkill -f gunicorn
     screen -d -r $SCREEN_NAME -X quit
     echo "Starte Flask-App in einer neuen Screen-Session..."
-    screen -AmdS "$SCREEN_NAME" /bin/bash -c "while true; do ./.venv/bin/gunicorn -w 2 --reload -b $HOST:5000 main:app; echo 'Website crashed. Restarting in 10 seconds...'; sleep 10; done"
+    screen -AmdS "$SCREEN_NAME" /bin/bash -c "while true; do ./.venv/bin/gunicorn -w 4 --reload -b $HOST:5000 main:app; echo 'Website crashed. Restarting in 10 seconds...'; sleep 10; done"
     echo "Flask-App läuft jetzt in der Screen-Session '$SCREEN_NAME'."
 else
     echo "Starte Flask-App in einer neuen Screen-Session..."
